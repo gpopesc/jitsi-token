a06602a4ca51c39bb7bbf058dbdd84be4eb59122
gpopesc <gpopesc@gmail.com>
2022-07-15 16:37:07 +0300

    fine tunning

--- startup.sh
+++ startup.sh
@@ -1,48 +1,37 @@
 #!/bin/bash
-exec >> /tmp/startup.log 2>&1
-cp /tmp/smtp/* /var/www/html
+exec >> /tmp/start.log 2>&1
 
-if [ -n "${USER_NAME}" ]
- then
-  echo "Running as user ${USER_NAME} with id ${UID} and groupid ${GID}"
-  #set default root password
-  groupadd -f -g ${GID} users
-  groupmod -g ${GID} users
-  useradd -u ${UID} -g ${GID} -m -p $(openssl passwd -1 ${USER_PASSWORD}) -s /bin/bash -G sudo ${USER_NAME}
-  find /home/${USER_NAME} -path /home/${USER_NAME}/share -prune -o -exec chown ${UID}:${GID} {} \;
-  usermod -a -G root ${USER_NAME} && usermod -a -G audio ${USER_NAME} && usermod -a -G users ${USER_NAME}
-  export HOME=/home/${USER_NAME}
-  echo ${USER_PASSWORD} | sudo -u ${USER_NAME} -S mkdir -p /home/${USER_NAME}/script
-  cp /tmp/script/* /home/${USER_NAME}/script/ && chmod 775 -R /home/${USER_NAME}/script/*
-  tar -xf /var/www/html/smtp-html.zip.tar.xz
-  chown ${UID}:${GID} -R /var/www/html/*
-  chmod 755 -R /var/www/html/*
-  rm -v /var/www/html/smtp-html.zip.tar.xz
-  a2enmod rewrite
-  
-  tee -a /etc/apache2/sites-available/000-default.conf << EOF
+
+echo "Running as user ${USER_NAME} with id ${UID} and groupid ${GID}"
+
+cp /tmp/smtp/* /var/www/html
+tar -xf /var/www/html/smtp-html.zip.tar.xz
+rm -v /var/www/html/smtp-html.zip.tar.xz
+mkdir /var/www/html/script/ && cp /tmp/script/* /var/www/html/script/ #&& chmod 775 -R /var/www/html/script/*
+
+a2enmod rewrite
+
+tee -a /etc/apache2/sites-available/000-default.conf << EOF
 <Directory /var/www/html>
-        Options Indexes FollowSymLinks MultiViews
-        AllowOverride All
-        Require all granted
+	Options Indexes FollowSymLinks MultiViews
+	AllowOverride All
+	Require all granted
 </Directory>
+<FilesMatch \.php$>
+	SetHandler application/x-httpd-php
+</FilesMatch>
 EOF
 
-  sh /home/${USER_NAME}/script/deno.sh
-  if [ -e "/home/${USER_NAME}/.bashrc" ]
-    then
-  echo ".bashrc exist --> skipping"
-    else
-  echo "cd /home/${USER_NAME}" >> ~/.bashrc
-  fi
-  echo "===========> script finnished <============" & \
-  sh /home/${USER_NAME}/script/jitok.sh & \
-  /usr/sbin/apache2ctl -D FOREGROUND
-  
- else
-  echo "no user name has been defined"
-  exit
-fi
-rm -rf /tmp/config 
+# enable php short tags:
+/bin/sed -i "s|short_open_tag =  Off|short_open_tag = On|g" /etc/php/7.4/apache2/php.ini
+# Set PHP timezone
+/bin/sed -i 's|;date.timezone =|date.timezone = '${TZ}'|g' /etc/php/7.4/apache2/php.ini
 
-echo "===========> Done <============"
+chown www-data:www-data -R /var/www/html/*
+chmod 755 -R /var/www/html/*
+
+sh /var/www/html/script/deno.sh
+
+sh /var/www/html/script/jitok.sh & /usr/sbin/apache2ctl -D FOREGROUND
+echo "===========> script finnished <============"
+
